08341b77f7155b0d937f6fa514ba17ce30773018
Ashu112 <kashu2611@gmail.com>
2023-12-21 02:39:04 +0530

    random image working

--- src/Components/DogPic.jsx
+++ src/Components/DogPic.jsx
@@ -1,32 +1,43 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import { useState } from "react";
+import axios from "axios";
+import styles from "./DogPic.module.css"
 
 const DogPic = () => {
-    const [breed, setBreed] = useState("random");
+  const [breed, setBreed] = useState("Random");
+  const [dogImage, setDogImage] = useState("");
+  const getDogImage = async () => {
+    let url = "https://dog.ceo/api/breeds/image/random";
+    const response = await axios.get(url);
+    setDogImage(response.data.message)
+  };
+  
+  useEffect(()=>{
+    getDogImage();
+  },[])
+
   return (
-    <div>
-      <div>
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
         <label>
-          Select a breed
+          Select a breed:
           <select
             value={breed}
             onChange={(e) => setBreed(e.target.value)}
-            defaultValue="random"
           >
-           <option value="Random">Random</option>
+            <option value="Random">Random</option>
             <option value="Beagle">Beagle</option>
             <option value="Boxer">Boxer</option>
             <option value="Dalmatian">Dalmatian</option>
             <option value="Husky">Husky</option>
           </select>
-          
         </label>
-        </div>
-        <img src="" alt="Dogimg"/>
-        <br/>
-        <button>Next</button>
       </div>
-  )
-}
+      <img src={dogImage} alt="Dogimg" />
+      <br />
+      <button>Next</button>
+    </div>
+  );
+};
 
-export default DogPic
+export default DogPic;
